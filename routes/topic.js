35e72d34d7168df773f2978c610f91e5f91be266
SahJ00 <salvador.henares@gmail.com>
2019-07-07 14:27:09 +0200

    Actualización 07072019 1426 arreglado algunos errores en las rutas

--- routes/topic.js
+++ routes/topic.js
@@ -8,7 +8,7 @@ var md_auth = require('../middlewares/authenticated');
 
 router.post('/topic', md_auth.authenticated, TopicController.save);
 router.get('/topics/:page?', TopicController.getTopics);
-router.get('/user-topics/:user?', TopicController.getTopicsByUser);
+router.get('/user-topics/:user', TopicController.getTopicsByUser);
 router.get('/topic/:id', TopicController.getTopic);
 router.put('/topic/:id', md_auth.authenticated, TopicController.update);
 router.delete('/topic/:id', md_auth.authenticated, TopicController.delete);
